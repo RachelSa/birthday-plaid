2a0f0b23306b4e3af2be1b39198eea26eac93032
Rachel Salois <rachel.salois@gmail.com>
2017-09-25 13:07:34 -0400

    improved color variation and moved form up

--- index.js
+++ index.js
@@ -8,53 +8,55 @@ function addColor(className, rgb){
 
 function convertMonth(month){
   first = month < 6 ? month * 35 : month
-  second = month + 100
-  third = month * 3
+  second = month > 9 ? month * 21 : month * 15
+  third = month % 2 == 0 ? month * 6 : month
   return `rgb(${first}, ${second}, ${third})`
 }
 
 function convertDay(day){
-  first = day * 2
-  second = day < 15 ? day : day + 200
+  first = day % 2 == 0 ? day * 2 : day * 6
+  second = day < 20 ? day + 50 : day + 200
   third = day < 5 ? day * 5 : day + 125
   return `rgb(${first}, ${second}, ${third})`
 }
 
 function convertYearFirst(year){
-  first = year < 20 ? year + 200 : year
+  first = year === 19 ? year + 200 : year
   second = year
   third = year * 2
   return `rgb(${first}, ${second}, ${third})`
 }
 
 function convertYearSecond(year){
-  first = year
+  first = year % 2 === 0 ? year : parseInt(year * 2.5)
   second = year > 80 ? year + 100 : year
-  third = year < 50 ? year + 200 : year
+  third = year < 20 ?  year : year + 120
   return `rgb(${first}, ${second}, ${third})`
 }
 
 function bodyColor(day, month, year){
-  first = day + 200
-  second = month + 210
-  third = year < 80 ? 255 : 255 - year
+  first = day + 224
+  second = month + 243
+  third = year < 20 ? 255 - year : 255
   document.body.style.backgroundColor = `rgb(${first}, ${second}, ${third})`
 }
 
 function addTemplate(day, month){
   let wrap = document.getElementById("wrapper")
-  let parsedMonth = month % 2 === 0 ? month * 2 : month * 3
+  let parsedMonth = day % 2 === 0 ? day * 2 : month * 4
   wrap.style.setProperty("grid-template-columns", `${day}px 1fr ${parsedMonth}px 1fr ${day}px 1fr ${parsedMonth}px 1fr`)
   wrap.style.setProperty("grid-template-rows", `${day}px 1fr ${parsedMonth}px 1fr ${day}px 1fr ${parsedMonth}px 1fr`)
 }
 
 function calcYearFirst(year){
-  let num = parseInt(year.substr(2,4))
+  let sub = year.substr(0,2)
+  let num = parseInt(sub)
   return num < 0 || num > 21 ? 20 : num
 }
 
 function calcYearSecond(year){
-  let num = parseInt(year.substr(2,4))
+  let sub = year.substr(2,4)
+  let num = parseInt(sub)
   return num < 0 || num > 100 ? 99 : num
 }
 
@@ -96,7 +98,7 @@ function changeCSS(year, month, day){
 }
 
 window.onload = () => {
-  changeCSS(year = "2000", month = "6", day = "30")
+  changeCSS(year = "1892", month = "05", day = "06")
   addFormListener()
 
 }
